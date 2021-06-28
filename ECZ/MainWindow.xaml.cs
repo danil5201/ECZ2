ed8893814b89de3a45078c0c3f8ee291445f548f
Даниил Панасенко <ponasenkodanil@yandex.ru>
2021-06-28 10:28:13 +0700

    Создан функционал добавления

--- ECZ/MainWindow.xaml.cs
+++ ECZ/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace ECZ
     /// </summary>
     public partial class MainWindow : Window
     {
+        gr693_pdvEntities1 db;
         public MainWindow()
         {
+            db = new gr693_pdvEntities1();
             InitializeComponent();
+            dgogrenci.ItemsSource = db.Employee.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.First_name = name.Text;
+            emp.Last_name = last.Text;
+            emp.Middle_name = mid.Text;
+            emp.Age = Convert.ToInt32(age.Text);
+            emp.Post = post.Text;
+            db.Employee.Add(emp);
+            db.SaveChanges();
+            dgogrenci.ItemsSource = db.Employee.ToList();
         }
     }
 }
